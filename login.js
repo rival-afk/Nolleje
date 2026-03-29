74abb29e6adbe7b2884f62c7004441c4c3a77506
itz-rival <afa76104103@gmail.com>
2026-03-29 21:21:49 +0300

    добавил иконку и еще чуть изменений

---
+++ login.js
@@ -0,0 +1,6 @@
+const loginTab = document.getElementById("login");
+const registerTab = document.getElementById("register");
+
+loginTab.addEventListener("click", function () {
+  console.log("Login")
+});
